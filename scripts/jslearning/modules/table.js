0bae170426032c340322884e5e439d8f92c4286c
toyoharu nishikawa <toyohal@email.com>
2019-01-20 01:22:09 +0900

    add gaussKernel and SVR

--- scripts/jslearning/modules/table.js
+++ scripts/jslearning/modules/table.js
@@ -4,10 +4,11 @@ import {plot} from "./plot.js"
 "use strict"
 export const table={
   point: null,
+  tabu: new Tabulator("#table",{height:"60px"}),
   cellEdit: async function(e){
     const predict = learning.predict
     const list = e.getRow().getCells().map(v=>v.getValue())
-    const point = list.slice(1,-1).map(parseFloat)
+    const point = list.slice(1).map(parseFloat)
 
     const y = await predict(point)
     e.getRow().getCell("target").setValue(y)
@@ -15,7 +16,7 @@ export const table={
 
   },
   getPoint:function(){
-    const cells = $("#table").tabulator("getData")
+    const cells = this.tabu.getData()
     const list = cells[0]
     const keys = Object.keys(list)
       .filter(v=>v.includes("value"))
@@ -26,43 +27,32 @@ export const table={
           return flag
         })
     const point = keys.map(v=>parseFloat(list[v]))
+    console.log("point",point)
     this.point = point
     return point
   },
   changeMethod:async function(){
+    const tabu = this.tabu
     const point = this.point
     const predict = learning.predict
     const y = await predict(point)
  
-    $("#table").tabulator("updateData",[{id:0, target:y}] )
+    tabu.updateData([{id:0, target:y}] )
   },
   setTable:async function(labels, point){
+    const tabu = this.tabu
     const predict = learning.predict
     const y = await predict(point)
-    if($("#table").tabulator()){
-        $("#table").tabulator("destroy")
-    }
-    const columns = labels.map((v,i)=> i>0 ? 
-      Object({title: v, field:`value${i-1}`,editor:"input",cellEdited:this.cellEdit}):
-      Object({title: v, field:`target`})
-    ).concat(Object({title:"id", field:"id", visible:false}))
-    console.log(columns)
-    $("#table").tabulator({
-      height:"60px",
-      columns:columns
-      
-    })
-    const text = point.map((v,i)=>`value${i}`)
-    const data = point.map(v=>v) 
-    const data2 = text.map((v,i)=>{
-      const d = {} 
-      d[v]= data[i]
-      return d
-    })
-    const data3 = Object.assign({target:y,id:0}, ...data2)
-    console.log(data2)
-    //$("#table").tabulator("setData",setData )
-    $("#table").tabulator("setData",[data3] )
+
+    const columns = labels.map((v,i)=> i===0 ? 
+      Object({title: v, field:`target`}):
+      Object({title: v, field:v, editor:"input",cellEdited:this.cellEdit})
+    )
+
+    const data = Object.assign({id:0,target:y,}, ...point.map((v,i)=>Object({[labels[i+1]]:v})))
+
+    tabu.setColumns(columns)
+    tabu.addData(data)
   }
 }
 
